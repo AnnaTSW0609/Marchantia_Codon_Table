dfab37191ad65f2690deac7451af907d8326b2ee
AnnaTSW0609 <38995185+AnnaTSW0609@users.noreply.github.com>
2020-05-21 20:16:05 +0800

    Create Codon_table.py

---
+++ Codon_table.py
@@ -0,0 +1,4 @@
+bases = "tcag"
+codons = [a + b + c for a in bases for b in bases for c in bases]
+amino_acids 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'
+codon_table = dict(zip(codons, amino_acids))
